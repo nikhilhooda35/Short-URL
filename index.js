dc3632ed44ad6a8f9d940e4c7b512dfc4f91be99
Nikhil Hooda <nikhil.hooda@uneecops.com>
2024-07-25 11:55:19 +0530

    Fix

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require("express");
-
+const path = require("path");
 const urlRoute = require("./routes/url");
 const { connectDB } = require("./connection");
 const URL = require("./models/url");
 const { handleGetAnalytics } = require("./controllers/url");
-
+const staticRouter = "./routes/staticRouter";
 const app = express();
 const PORT = 8001;
 
 connectDB();
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 app.use(express.json());
+
+app.get("/test", async (req, res) => {
+  const allUrls = await URL.find({});
+  return res.render("home", {
+    urls: allUrls,
+  });
+});
+
+app.use("/", staticRouter);
 
 app.use("/url", urlRoute);
 
@@ -26,7 +38,10 @@ app.get("/:shortId", async (req, res) => {
       },
     }
   );
-  res.redirect(entry.redirectURL);
+  const appendURL = entry?.redirectURL.includes("http")
+    ? entry.redirectURL
+    : `https://${entry?.redirectURL}`;
+  res.redirect(appendURL);
 });
 
 app.get("/analytics/:shortId", handleGetAnalytics);
